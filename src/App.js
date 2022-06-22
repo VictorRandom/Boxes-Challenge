89aeefa7eae78e037131a1f8bb91dc495852fadf
VictorRandom <victormbalduino@hotmail.com>
2022-06-22 09:33:03 -0300

    creating boxes style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from "react"
+import React, {useState} from "react"
 import boxes from "./boxes"
 
-export default function App() {
+export default function App(props) {
 
+    const [allBoxes, setAllBoxes] = useState(boxes);
+
+    const darkMode = props.darkMode;
+    
+    const styles = {
+        backgroundColor: darkMode ? "#222222" : "#cccccc"
+    }
+
+    const boxesElements = allBoxes.map(box => ( 
+        <div style={styles} className='box' key={box.id}></div>
+    ))
 
     return (
         <main>
-            <h1>Boxes will go here</h1>
+            {boxesElements}
         </main>
     )
 }
